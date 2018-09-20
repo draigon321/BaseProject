a4532948623494979e57ed70977a7549bc83f617
Draigon <dhwatson321@gmail.com>
2018-09-20 04:14:03 -0800

    Script Inspector
    Added Script inspector

---
+++ Assets/_MyAssets/Component_Exists.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+//#if UNITY_EDITOR
+using UnityEditor;
+
+public class Component_Exists : MonoBehaviour {
+	//public  GameObject gameObjectToCheck;
+
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	public bool ComponentExists (GameObject gameObjectToCheck)
+	{
+		bool x = false;
+		if (gameObjectToCheck.GetComponent<Renderer>() != null)
+	        
+		{
+			x=true;
+		}
+		return x;
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/_MyAssets/GetSize.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+//#if UNITY_EDITOR
+using UnityEditor;
+
+public class GetSize : MonoBehaviour {
+	[SerializeField]
+	private Vector3 x;
+	public GameObject targetGameObject;
+    // Use this for initialization
+	void Start () 
+		{
+
+		}
+	
+	public Vector3 ComponentExists (GameObject targetGameObject)
+		{
+			 //= false;
+			if (targetGameObject.GetComponent<Renderer>() != null)
+			{
+				Bounds xBounds=targetGameObject.GetComponent<Renderer>().bounds;
+				x=xBounds.size;
+			}
+			else
+			{
+				Bounds xBounds=targetGameObject.GetComponent<Collider>().bounds;
+				x=xBounds.size;
+			}
+			return x;
+	    }
+	// Update is called once per frame
+	void Update () 
+	{
+		//if (targetGameObject.GetComponent<Renderer>() != null)
+			//{
+			//Bounds xBounds=targetGameObject.GetComponent<Renderer>().bounds;
+			//	x=xBounds.size;
+			//}
+		//else
+		//{
+			//Bounds xBounds=targetGameObject.GetComponent<Collider>().bounds;
+			//x=xBounds.size;
+			//		}
+		//Debug.Log(x);
+	}
+	
+}
